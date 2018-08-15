c7f8c009978863ba6bbbc7e4f21a364e7542aa37
Eric Laugier <ela@egen.guru>
2018-08-15 03:01:02 +0200

    add dbconnection

--- cmd/lpdp-api-server/main.go
+++ cmd/lpdp-api-server/main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/elaugier/lpdp/pkg/models"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
+//DATABASEDRIVER ...
+var DATABASEDRIVER = os.Getenv("DATABASEDRIVER")
+
+//DATABASEHOSTNAME ...
+var DATABASEHOSTNAME = os.Getenv("DATABASEHOSTNAME")
+
+//DATABASEPORT ...
+var DATABASEPORT = os.Getenv("DATABASEPORT")
+
+//DATABASENAME ...
+var DATABASENAME = os.Getenv("DATABASENAME")
+
+//DATABASEUSERNAME ...
+var DATABASEUSERNAME = os.Getenv("DATABASEUSERNAME")
+
+//DATABASEPASSWORD ...
+var DATABASEPASSWORD = os.Getenv("DATABASEPASSWORD")
+
 func main() {
+	conn, err := gorm.Open(DATABASEDRIVER, fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s",
+		DATABASEHOSTNAME, DATABASEPORT, DATABASEUSERNAME, DATABASENAME, DATABASEPASSWORD))
+	if err != nil {
+		fmt.Printf("couldn't connect to database: %v\n", err)
+		return
+	}
+	defer conn.Close()
+
 	var p models.User
 	p.ID = "test"
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
--- pkg/models/models.go
+++ pkg/models/models.go
@@ -2,29 +2,62 @@ package models
 
 import (
 	"time"
-
-	"github.com/jinzhu/gorm"
 )
 
-//User ...
-type User struct {
-	gorm.Model
+//Achievement ...
+type Achievement struct {
 	ID        string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	RemovedAt time.Time
-	Email     string
+}
+
+func (Achievement) tableName() string {
+	return "achievements"
+}
+
+//Activity ...
+type Activity struct {
+	ID        string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	RemovedAt time.Time
+}
+
+//TableName ...
+func (Activity) TableName() string {
+	return "activities"
+}
+
+//User ...
+type User struct {
+	ID        string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	RemovedAt time.Time
+	Email     string `gorm:"type:varchar(100);unique_index"`
 	UserName  string
 	Password  string
 	FirstName string
 	LastName  string
+	BirthDate time.Time
+	Gender    uint
+}
+
+//TableName ...
+func (User) TableName() string {
+	return "users"
 }
 
 //Warning ...
 type Warning struct {
-	gorm.Model
 	ID        string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	RemovedAt time.Time
+}
+
+//TableName ...
+func (Warning) TableName() string {
+	return "warnings"
 }
