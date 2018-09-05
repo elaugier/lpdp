32c6290ccbaab2c0697be40a619219699204b6a3
Eric Laugier <ela@egen.guru>
2018-09-05 05:15:23 +0200

    Update ui to angular

---
+++ src-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lpdp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'lpdp-ui';
+}
