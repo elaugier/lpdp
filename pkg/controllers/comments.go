2ac0cce77845e8cb41e793a799384c04213c180e
Eric Laugier <ela@egen.guru>
2018-09-30 17:57:45 +0200

    update controllers

--- pkg/controllers/comments.go
+++ pkg/controllers/comments.go
@@ -3,8 +3,11 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/elaugier/lpdp/pkg/db"
+	"github.com/elaugier/lpdp/pkg/logs"
 	"github.com/elaugier/lpdp/pkg/models"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 //CommentsController ...
@@ -12,7 +15,26 @@ type CommentsController struct{}
 
 //Get ...
 func (u CommentsController) Get(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+	log := logs.GetInstance()
+	log.Println("try to retieve 'id' in url path")
+	id, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(400, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+	var i models.Comment
+	conn := db.GetInstance()
+	if err = conn.Where("id = ?", id).First(&i).Error; err != nil {
+		log.Println("comment not found.")
+		c.JSON(404, gin.H{
+			"msg": "comment not found.",
+		})
+	} else {
+		log.Println("comment returned")
+		c.JSON(200, i)
+	}
 }
 
 //List ...
--- pkg/controllers/correctionrequestactions.go
+++ pkg/controllers/correctionrequestactions.go
@@ -3,8 +3,11 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/elaugier/lpdp/pkg/db"
+	"github.com/elaugier/lpdp/pkg/logs"
 	"github.com/elaugier/lpdp/pkg/models"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 //CorrectionRequestActionsController ...
@@ -12,7 +15,26 @@ type CorrectionRequestActionsController struct{}
 
 //Get ...
 func (u CorrectionRequestActionsController) Get(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+	log := logs.GetInstance()
+	log.Println("try to retieve 'id' in url path")
+	id, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(400, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+	var i models.CorrectionRequestAction
+	conn := db.GetInstance()
+	if err = conn.Where("id = ?", id).First(&i).Error; err != nil {
+		log.Println("action not found.")
+		c.JSON(404, gin.H{
+			"msg": "action not found.",
+		})
+	} else {
+		log.Println("action returned")
+		c.JSON(200, i)
+	}
 }
 
 //List ...
