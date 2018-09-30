42ffb7b5d99ac6cfa42d39f9cebf2a0471efbb97
Eric Laugier <ela@egen.guru>
2018-09-30 17:49:18 +0200

    update controllers

--- pkg/controllers/badgetypes.go
+++ pkg/controllers/badgetypes.go
@@ -3,8 +3,11 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/elaugier/lpdp/pkg/db"
+	"github.com/elaugier/lpdp/pkg/logs"
 	"github.com/elaugier/lpdp/pkg/models"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 //BadgeTypesController ...
@@ -12,7 +15,26 @@ type BadgeTypesController struct{}
 
 //Get ...
 func (u BadgeTypesController) Get(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+	log := logs.GetInstance()
+	log.Println("try to retieve 'id' in url path")
+	id, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(400, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+	var i models.BadgeType
+	conn := db.GetInstance()
+	if err = conn.Where("id = ?", id).First(&i).Error; err != nil {
+		log.Println("badge type not found.")
+		c.JSON(404, gin.H{
+			"msg": "badge type not found.",
+		})
+	} else {
+		log.Println("badge type returned")
+		c.JSON(200, i)
+	}
 }
 
 //List ...
--- pkg/controllers/badipaddresses.go
+++ pkg/controllers/badipaddresses.go
@@ -3,8 +3,11 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/elaugier/lpdp/pkg/db"
+	"github.com/elaugier/lpdp/pkg/logs"
 	"github.com/elaugier/lpdp/pkg/models"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 //BadIPAddressesController ...
@@ -12,7 +15,26 @@ type BadIPAddressesController struct{}
 
 //Get ...
 func (u BadIPAddressesController) Get(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+	log := logs.GetInstance()
+	log.Println("try to retieve 'id' in url path")
+	id, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(400, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+	var i models.BadIPAddress
+	conn := db.GetInstance()
+	if err = conn.Where("id = ?", id).First(&i).Error; err != nil {
+		log.Println("ip not found.")
+		c.JSON(404, gin.H{
+			"msg": "ip not found.",
+		})
+	} else {
+		log.Println("ip returned")
+		c.JSON(200, i)
+	}
 }
 
 //List ...
