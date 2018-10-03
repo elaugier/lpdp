6e90dccbce0f1fed4d8bc116c3bee6d84e66aada
Eric Laugier <ela@egen.guru>
2018-10-03 08:39:12 +0200

    update final version of achievements and activities controllers

--- pkg/controllers/achievements.go
+++ pkg/controllers/achievements.go
@@ -3,13 +3,11 @@ package controllers
 import (
 	"net/http"
 
-	"github.com/elaugier/lpdp/pkg/logs"
-
-	"github.com/google/uuid"
-
 	"github.com/elaugier/lpdp/pkg/db"
+	"github.com/elaugier/lpdp/pkg/logs"
 	"github.com/elaugier/lpdp/pkg/models"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 //AchievementsController ...
@@ -17,80 +15,139 @@ type AchievementsController struct{}
 
 //Get ...
 func (u AchievementsController) Get(c *gin.Context) {
+
 	log := logs.GetInstance()
+
 	log.Println("try to retieve 'id' in url path")
+
 	id, err := uuid.Parse(c.Params.ByName("id"))
 	if err != nil {
 		log.Println("Cannot get 'id' in url path.")
-		c.JSON(400, gin.H{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"msg": "Cannot get 'id' in url path.",
 		})
 	}
+
 	var achievement models.Achievement
+
 	conn := db.GetInstance()
+
 	if err = conn.Where("id = ?", id).First(&achievement).Error; err != nil {
 		log.Println("achievement not found.")
-		c.JSON(404, gin.H{
+		c.JSON(http.StatusNotFound, gin.H{
 			"msg": "achievement not found.",
 		})
 	} else {
 		log.Println("achievement returned")
-		c.JSON(200, achievement)
+		c.JSON(http.StatusOK, achievement)
 	}
+
 }
 
 //List ...
 func (u AchievementsController) List(c *gin.Context) {
+
 	log := logs.GetInstance()
+
 	var achievements []models.Achievement
+
 	conn := db.GetInstance()
 	if err := conn.Find(&achievements).Error; err != nil {
 		log.Println("achievement not found.")
-		c.JSON(404, gin.H{
+		c.JSON(http.StatusNotFound, gin.H{
 			"msg": "achievement not found.",
 		})
 	} else {
 		log.Println("achievement returned")
-		c.JSON(200, achievements)
+		c.JSON(http.StatusOK, achievements)
 	}
+
 }
 
 //Add ...
 func (u AchievementsController) Add(c *gin.Context) {
-	var json models.Achievement
-	if err := c.ShouldBindJSON(&json); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	c.String(http.StatusOK, "Working!")
-}
 
-//Modify ...
-func (u AchievementsController) Modify(c *gin.Context) {
-	log := logs.GetInstance()
 	var achievement models.Achievement
-	id, err := uuid.Parse(c.Params.ByName("id"))
-	if err != nil {
-		log.Println("Cannot get 'id' in url path.")
-		c.JSON(400, gin.H{
-			"msg": "Cannot get 'id' in url path.",
-		})
-	}
-	conn := db.GetInstance()
-	if err = conn.Where("id = ?", id).First(&achievement).Error; err != nil {
-		c.AbortWithStatus(http.StatusNotFound)
-		log.Println("achievement not found.")
-	}
+
 	if err := c.ShouldBindJSON(&achievement); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.BindJSON(&achievement)
+
+	if err := c.BindJSON(&achievement); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn := db.GetInstance()
 	conn.Save(&achievement)
-	c.JSON(200, achievement)
+
+	c.JSON(http.StatusCreated, achievement)
+
+}
+
+//Modify ...
+func (u AchievementsController) Modify(c *gin.Context) {
+
+	log := logs.GetInstance()
+
+	var achievement models.Achievement
+
+	id, err := uuid.Parse(c.Params.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+
+	conn := db.GetInstance()
+
+	if err = conn.Where("id = ?", id).First(&achievement).Error; err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		log.Println("achievement not found.")
+	}
+
+	if err := c.ShouldBindJSON(&achievement); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := c.BindJSON(&achievement); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn.Save(&achievement)
+
+	c.JSON(http.StatusOK, achievement)
 }
 
 //Remove ...
 func (u AchievementsController) Remove(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+
+	log := logs.GetInstance()
+
+	id, err := uuid.Parse(c.Params.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+
+	var achievement models.Achievement
+
+	conn := db.GetInstance()
+
+	if err = conn.Where("id = ?", id).Delete(&achievement).Error; err != nil {
+		log.Println("achievement not found.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "achievement not found.",
+		})
+	} else {
+		log.Println("achievement returned")
+		c.JSON(http.StatusOK, achievement)
+	}
+
 }
--- pkg/controllers/activities.go
+++ pkg/controllers/activities.go
@@ -3,13 +3,11 @@ package controllers
 import (
 	"net/http"
 
-	"github.com/elaugier/lpdp/pkg/logs"
-
-	"github.com/google/uuid"
-
 	"github.com/elaugier/lpdp/pkg/db"
+	"github.com/elaugier/lpdp/pkg/logs"
 	"github.com/elaugier/lpdp/pkg/models"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 //ActivitiesController ...
@@ -17,65 +15,139 @@ type ActivitiesController struct{}
 
 //Get ...
 func (u ActivitiesController) Get(c *gin.Context) {
+
 	log := logs.GetInstance()
+
 	log.Println("try to retieve 'id' in url path")
+
 	id, err := uuid.Parse(c.Params.ByName("id"))
 	if err != nil {
 		log.Println("Cannot get 'id' in url path.")
-		c.JSON(400, gin.H{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"msg": "Cannot get 'id' in url path.",
 		})
 	}
+
 	var activity models.Activity
+
 	conn := db.GetInstance()
+
 	if err = conn.Where("id = ?", id).First(&activity).Error; err != nil {
 		log.Println("activity not found.")
-		c.JSON(404, gin.H{
+		c.JSON(http.StatusNotFound, gin.H{
 			"msg": "activity not found.",
 		})
 	} else {
 		log.Println("activity returned")
-		c.JSON(200, activity)
+		c.JSON(http.StatusOK, activity)
 	}
+
 }
 
 //List ...
 func (u ActivitiesController) List(c *gin.Context) {
+
 	log := logs.GetInstance()
+
 	var activities []models.Activity
+
 	conn := db.GetInstance()
 	if err := conn.Find(&activities).Error; err != nil {
 		log.Println("activity not found.")
-		c.JSON(404, gin.H{
+		c.JSON(http.StatusNotFound, gin.H{
 			"msg": "activity not found.",
 		})
 	} else {
 		log.Println("activity returned")
-		c.JSON(200, activities)
+		c.JSON(http.StatusOK, activities)
 	}
+
 }
 
 //Add ...
 func (u ActivitiesController) Add(c *gin.Context) {
-	var json models.Activity
-	if err := c.ShouldBindJSON(&json); err != nil {
+
+	var activity models.Activity
+
+	if err := c.ShouldBindJSON(&activity); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.String(http.StatusOK, "Working!")
+
+	if err := c.BindJSON(&activity); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn := db.GetInstance()
+	conn.Save(&activity)
+
+	c.JSON(http.StatusCreated, activity)
+
 }
 
 //Modify ...
 func (u ActivitiesController) Modify(c *gin.Context) {
-	var json models.Activity
-	if err := c.ShouldBindJSON(&json); err != nil {
+
+	log := logs.GetInstance()
+
+	var activity models.Activity
+
+	id, err := uuid.Parse(c.Params.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path")
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Cannot get 'id' in url path",
+		})
+	}
+
+	conn := db.GetInstance()
+
+	if err := conn.Where("id = ?", id).First(&activity).Error; err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		log.Println("activity not found")
+	}
+
+	if err := c.ShouldBindJSON(&activity); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.String(http.StatusOK, "Working!")
+
+	if err := c.BindJSON(&activity); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn.Save(&activity)
+
+	c.JSON(http.StatusOK, activity)
 }
 
 //Remove ...
 func (u ActivitiesController) Remove(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+
+	log := logs.GetInstance()
+
+	id, err := uuid.Parse(c.Params.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+
+	var activity models.Activity
+
+	conn := db.GetInstance()
+
+	if err = conn.Where("id = ?", id).Delete(&activity).Error; err != nil {
+		log.Println("activity not found.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "activity not found.",
+		})
+	} else {
+		log.Println("activity returned")
+		c.JSON(http.StatusOK, activity)
+	}
+
 }
