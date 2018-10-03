7c9b5ea6fe3802e409f72a223957f22aff0038d3
Eric Laugier <ela@egen.guru>
2018-10-03 23:53:05 +0200

    update alert actions

--- pkg/controllers/alertactions.go
+++ pkg/controllers/alertactions.go
@@ -3,13 +3,11 @@ package controllers
 import (
 	"net/http"
 
-	"github.com/elaugier/lpdp/pkg/logs"
-
-	"github.com/google/uuid"
-
 	"github.com/elaugier/lpdp/pkg/db"
+	"github.com/elaugier/lpdp/pkg/logs"
 	"github.com/elaugier/lpdp/pkg/models"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 //AlertActionsController ...
@@ -17,65 +15,139 @@ type AlertActionsController struct{}
 
 //Get ...
 func (u AlertActionsController) Get(c *gin.Context) {
+
 	log := logs.GetInstance()
+
 	log.Println("try to retieve 'id' in url path")
+
 	id, err := uuid.Parse(c.Params.ByName("id"))
 	if err != nil {
 		log.Println("Cannot get 'id' in url path.")
-		c.JSON(400, gin.H{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"msg": "Cannot get 'id' in url path.",
 		})
 	}
+
 	var alertaction models.AlertAction
+
 	conn := db.GetInstance()
+
 	if err = conn.Where("id = ?", id).First(&alertaction).Error; err != nil {
-		log.Println("alert not found.")
-		c.JSON(404, gin.H{
-			"msg": "alert not found.",
+		log.Println("alert action not found.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "alert action not found.",
 		})
 	} else {
-		log.Println("alert returned")
-		c.JSON(200, alertaction)
+		log.Println("alert action returned")
+		c.JSON(http.StatusOK, alertaction)
 	}
+
 }
 
 //List ...
 func (u AlertActionsController) List(c *gin.Context) {
+
 	log := logs.GetInstance()
+
 	var alertactions []models.AlertAction
+
 	conn := db.GetInstance()
 	if err := conn.Find(&alertactions).Error; err != nil {
-		log.Println("action not found.")
-		c.JSON(404, gin.H{
-			"msg": "action not found.",
+		log.Println("alert action not found.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "alert action not found.",
 		})
 	} else {
-		log.Println("action returned")
-		c.JSON(200, alertactions)
+		log.Println("alert action returned")
+		c.JSON(http.StatusOK, alertactions)
 	}
+
 }
 
 //Add ...
 func (u AlertActionsController) Add(c *gin.Context) {
-	var json models.AlertAction
-	if err := c.ShouldBindJSON(&json); err != nil {
+
+	var alertactions models.AlertAction
+
+	if err := c.ShouldBindJSON(&alertactions); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.String(http.StatusOK, "Working!")
+
+	if err := c.BindJSON(&alertactions); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn := db.GetInstance()
+	conn.Save(&alertactions)
+
+	c.JSON(http.StatusCreated, alertactions)
+
 }
 
 //Modify ...
 func (u AlertActionsController) Modify(c *gin.Context) {
-	var json models.AlertAction
-	if err := c.ShouldBindJSON(&json); err != nil {
+
+	log := logs.GetInstance()
+
+	var alertactions models.AlertAction
+
+	id, err := uuid.Parse(c.Params.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+
+	conn := db.GetInstance()
+
+	if err = conn.Where("id = ?", id).First(&alertactions).Error; err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		log.Println("alert action not found.")
+	}
+
+	if err := c.ShouldBindJSON(&alertactions); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.String(http.StatusOK, "Working!")
+
+	if err := c.BindJSON(&alertactions); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn.Save(&alertactions)
+
+	c.JSON(http.StatusOK, alertactions)
 }
 
 //Remove ...
 func (u AlertActionsController) Remove(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+
+	log := logs.GetInstance()
+
+	id, err := uuid.Parse(c.Params.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+
+	var alertaction models.AlertAction
+
+	conn := db.GetInstance()
+
+	if err = conn.Where("id = ?", id).Delete(&alertaction).Error; err != nil {
+		log.Println("alert action not found.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "alert action not found.",
+		})
+	} else {
+		log.Println("alert action returned")
+		c.JSON(http.StatusOK, alertaction)
+	}
+
 }
