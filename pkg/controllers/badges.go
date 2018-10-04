ba9e29756e64c665bffd3f03fafb261549853389
Eric Laugier <ela@egen.guru>
2018-10-05 01:38:28 +0200

    update badges controller

--- pkg/controllers/badges.go
+++ pkg/controllers/badges.go
@@ -15,54 +15,139 @@ type BadgesController struct{}
 
 //Get ...
 func (u BadgesController) Get(c *gin.Context) {
+
 	log := logs.GetInstance()
+
 	log.Println("try to retieve 'id' in url path")
+
 	id, err := uuid.Parse(c.Params.ByName("id"))
 	if err != nil {
 		log.Println("Cannot get 'id' in url path.")
-		c.JSON(400, gin.H{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"msg": "Cannot get 'id' in url path.",
 		})
 	}
-	var i models.Badge
+
+	var badge models.Badge
+
 	conn := db.GetInstance()
-	if err = conn.Where("id = ?", id).First(&i).Error; err != nil {
+
+	if err = conn.Where("id = ?", id).First(&badge).Error; err != nil {
 		log.Println("badge not found.")
-		c.JSON(404, gin.H{
+		c.JSON(http.StatusNotFound, gin.H{
 			"msg": "badge not found.",
 		})
 	} else {
 		log.Println("badge returned")
-		c.JSON(200, i)
+		c.JSON(http.StatusOK, badge)
 	}
+
 }
 
 //List ...
 func (u BadgesController) List(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+
+	log := logs.GetInstance()
+
+	var badges []models.Badge
+
+	conn := db.GetInstance()
+	if err := conn.Find(&badges).Error; err != nil {
+		log.Println("badge not found.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "badge not found.",
+		})
+	} else {
+		log.Println("badge returned")
+		c.JSON(http.StatusOK, badges)
+	}
+
 }
 
 //Add ...
 func (u BadgesController) Add(c *gin.Context) {
-	var json models.Badge
-	if err := c.ShouldBindJSON(&json); err != nil {
+
+	var badge models.Badge
+
+	if err := c.ShouldBindJSON(&badge); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.String(http.StatusOK, "Working!")
+
+	if err := c.BindJSON(&badge); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn := db.GetInstance()
+	conn.Save(&badge)
+
+	c.JSON(http.StatusCreated, badge)
+
 }
 
 //Modify ...
 func (u BadgesController) Modify(c *gin.Context) {
-	var json models.Badge
-	if err := c.ShouldBindJSON(&json); err != nil {
+
+	log := logs.GetInstance()
+
+	var badge models.Badge
+
+	id, err := uuid.Parse(c.Params.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+
+	conn := db.GetInstance()
+
+	if err = conn.Where("id = ?", id).First(&badge).Error; err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		log.Println("badge not found.")
+	}
+
+	if err := c.ShouldBindJSON(&badge); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.String(http.StatusOK, "Working!")
+
+	if err := c.BindJSON(&badge); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn.Save(&badge)
+
+	c.JSON(http.StatusOK, badge)
 }
 
 //Remove ...
 func (u BadgesController) Remove(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+
+	log := logs.GetInstance()
+
+	id, err := uuid.Parse(c.Params.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+
+	var badge models.Badge
+
+	conn := db.GetInstance()
+
+	if err = conn.Where("id = ?", id).Delete(&badge).Error; err != nil {
+		log.Println("badge not found.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "badge not found.",
+		})
+	} else {
+		log.Println("badge returned")
+		c.JSON(http.StatusOK, badge)
+	}
+
 }
