dbe570ea332bb327f84823e973d42e293a415d44
Eric Laugier <ela@egen.guru>
2018-10-06 15:16:07 +0200

    fix#12 fix#13

--- pkg/controllers/coauthors.go
+++ pkg/controllers/coauthors.go
@@ -10,59 +10,144 @@ import (
 	"github.com/google/uuid"
 )
 
-//CoauthorsController ...
-type CoauthorsController struct{}
+//CoAuthorsController ...
+type CoAuthorsController struct{}
 
 //Get ...
-func (ca CoauthorsController) Get(c *gin.Context) {
+func (u CoAuthorsController) Get(c *gin.Context) {
+
 	log := logs.GetInstance()
+
 	log.Println("try to retieve 'id' in url path")
+
 	id, err := uuid.Parse(c.Params.ByName("id"))
 	if err != nil {
 		log.Println("Cannot get 'id' in url path.")
-		c.JSON(400, gin.H{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"msg": "Cannot get 'id' in url path.",
 		})
 	}
-	var i models.CoAuthor
+
+	var coauthor models.CoAuthor
+
 	conn := db.GetInstance()
-	if err = conn.Where("id = ?", id).First(&i).Error; err != nil {
+
+	if err = conn.Where("id = ?", id).First(&coauthor).Error; err != nil {
 		log.Println("coauthor not found.")
-		c.JSON(404, gin.H{
+		c.JSON(http.StatusNotFound, gin.H{
 			"msg": "coauthor not found.",
 		})
 	} else {
 		log.Println("coauthor returned")
-		c.JSON(200, i)
+		c.JSON(http.StatusOK, coauthor)
 	}
+
 }
 
 //List ...
-func (ca CoauthorsController) List(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+func (u CoAuthorsController) List(c *gin.Context) {
+
+	log := logs.GetInstance()
+
+	var coauthors []models.CoAuthor
+
+	conn := db.GetInstance()
+	if err := conn.Find(&coauthors).Error; err != nil {
+		log.Println("coauthor not found.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "coauthor not found.",
+		})
+	} else {
+		log.Println("coauthor returned")
+		c.JSON(http.StatusOK, coauthors)
+	}
+
 }
 
 //Add ...
-func (ca CoauthorsController) Add(c *gin.Context) {
-	var json models.CoAuthor
-	if err := c.ShouldBindJSON(&json); err != nil {
+func (u CoAuthorsController) Add(c *gin.Context) {
+
+	var coauthor models.CoAuthor
+
+	if err := c.ShouldBindJSON(&coauthor); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.String(http.StatusOK, "Working!")
+
+	if err := c.BindJSON(&coauthor); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn := db.GetInstance()
+	conn.Save(&coauthor)
+
+	c.JSON(http.StatusCreated, coauthor)
+
 }
 
 //Modify ...
-func (ca CoauthorsController) Modify(c *gin.Context) {
-	var json models.CoAuthor
-	if err := c.ShouldBindJSON(&json); err != nil {
+func (u CoAuthorsController) Modify(c *gin.Context) {
+
+	log := logs.GetInstance()
+
+	var coauthor models.CoAuthor
+
+	id, err := uuid.Parse(c.Params.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+
+	conn := db.GetInstance()
+
+	if err = conn.Where("id = ?", id).First(&coauthor).Error; err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		log.Println("coauthor not found.")
+	}
+
+	if err := c.ShouldBindJSON(&coauthor); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.String(http.StatusOK, "Working!")
+
+	if err := c.BindJSON(&coauthor); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn.Save(&coauthor)
+
+	c.JSON(http.StatusOK, coauthor)
 }
 
 //Remove ...
-func (ca CoauthorsController) Remove(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+func (u CoAuthorsController) Remove(c *gin.Context) {
+
+	log := logs.GetInstance()
+
+	id, err := uuid.Parse(c.Params.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+
+	var coauthor models.CoAuthor
+
+	conn := db.GetInstance()
+
+	if err = conn.Where("id = ?", id).Delete(&coauthor).Error; err != nil {
+		log.Println("coauthor not found.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "coauthor not found.",
+		})
+	} else {
+		log.Println("coauthor returned")
+		c.JSON(http.StatusOK, coauthor)
+	}
+
 }
--- pkg/controllers/comments.go
+++ pkg/controllers/comments.go
@@ -15,54 +15,139 @@ type CommentsController struct{}
 
 //Get ...
 func (u CommentsController) Get(c *gin.Context) {
+
 	log := logs.GetInstance()
+
 	log.Println("try to retieve 'id' in url path")
+
 	id, err := uuid.Parse(c.Params.ByName("id"))
 	if err != nil {
 		log.Println("Cannot get 'id' in url path.")
-		c.JSON(400, gin.H{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"msg": "Cannot get 'id' in url path.",
 		})
 	}
-	var i models.Comment
+
+	var comment models.Comment
+
 	conn := db.GetInstance()
-	if err = conn.Where("id = ?", id).First(&i).Error; err != nil {
+
+	if err = conn.Where("id = ?", id).First(&comment).Error; err != nil {
 		log.Println("comment not found.")
-		c.JSON(404, gin.H{
+		c.JSON(http.StatusNotFound, gin.H{
 			"msg": "comment not found.",
 		})
 	} else {
 		log.Println("comment returned")
-		c.JSON(200, i)
+		c.JSON(http.StatusOK, comment)
 	}
+
 }
 
 //List ...
 func (u CommentsController) List(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+
+	log := logs.GetInstance()
+
+	var comments []models.Comment
+
+	conn := db.GetInstance()
+	if err := conn.Find(&comments).Error; err != nil {
+		log.Println("comment not found.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "comment not found.",
+		})
+	} else {
+		log.Println("comment returned")
+		c.JSON(http.StatusOK, comments)
+	}
+
 }
 
 //Add ...
 func (u CommentsController) Add(c *gin.Context) {
-	var json models.Comment
-	if err := c.ShouldBindJSON(&json); err != nil {
+
+	var comment models.Comment
+
+	if err := c.ShouldBindJSON(&comment); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.String(http.StatusOK, "Working!")
+
+	if err := c.BindJSON(&comment); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn := db.GetInstance()
+	conn.Save(&comment)
+
+	c.JSON(http.StatusCreated, comment)
+
 }
 
 //Modify ...
 func (u CommentsController) Modify(c *gin.Context) {
-	var json models.Comment
-	if err := c.ShouldBindJSON(&json); err != nil {
+
+	log := logs.GetInstance()
+
+	var comment models.Comment
+
+	id, err := uuid.Parse(c.Params.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+
+	conn := db.GetInstance()
+
+	if err = conn.Where("id = ?", id).First(&comment).Error; err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		log.Println("comment not found.")
+	}
+
+	if err := c.ShouldBindJSON(&comment); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.String(http.StatusOK, "Working!")
+
+	if err := c.BindJSON(&comment); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn.Save(&comment)
+
+	c.JSON(http.StatusOK, comment)
 }
 
 //Remove ...
 func (u CommentsController) Remove(c *gin.Context) {
-	c.String(http.StatusOK, "Working!")
+
+	log := logs.GetInstance()
+
+	id, err := uuid.Parse(c.Params.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get 'id' in url path.")
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Cannot get 'id' in url path.",
+		})
+	}
+
+	var comment models.Comment
+
+	conn := db.GetInstance()
+
+	if err = conn.Where("id = ?", id).Delete(&comment).Error; err != nil {
+		log.Println("comment not found.")
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "comment not found.",
+		})
+	} else {
+		log.Println("comment returned")
+		c.JSON(http.StatusOK, comment)
+	}
+
 }
