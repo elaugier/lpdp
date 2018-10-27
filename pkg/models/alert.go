ae9d23ccbb97166d1c41ec1b9896eb4094cf8c6a
Eric Laugier <ela@egen.guru>
2018-10-27 07:58:11 +0200

    update models

--- pkg/models/alert.go
+++ pkg/models/alert.go
@@ -12,9 +12,9 @@ type Alert struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	DeletedAt time.Time `json:"deleted_at"`
-	Type      string    `gorm:"type:varchar(100)"`
-	Details   string    `gorm:"type:varchar(2000)"`
-	User      string    `sql:"type:uuid"`
+	Type      string    `gorm:"type:varchar(100)" json:"type"`
+	Details   string    `gorm:"type:varchar(2000)" json:"details"`
+	UserRef   string    `sql:"type:uuid" json:"user_ref"`
 }
 
 //TableName ...
