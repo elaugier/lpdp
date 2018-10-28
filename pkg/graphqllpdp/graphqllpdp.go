e8b6ad6de7646a5061a0fb99ce234408d5fa4742
Eric Laugier <ela@egen.guru>
2018-10-28 11:49:52 +0100

    update graphql schema

--- pkg/graphqllpdp/graphqllpdp.go
+++ pkg/graphqllpdp/graphqllpdp.go
@@ -10,6 +10,7 @@ var (
 
 // GetSchema ...
 func GetSchema() (graphql.Schema, error) {
+
 	achievementInterface := graphql.NewInterface(graphql.InterfaceConfig{
 		Name:        "Achievement",
 		Description: "Achievement for LPDP Member",
@@ -30,8 +31,52 @@ func GetSchema() (graphql.Schema, error) {
 				Type:        graphql.NewNonNull(graphql.DateTime),
 				Description: "deletion date of achievement",
 			},
+			"Title": &graphql.Field{
+				Type:        graphql.String,
+				Description: "title of achievement",
+			},
+			"OwnerRef": &graphql.Field{
+				Type:        graphql.String,
+				Description: "owner of achievement",
+			},
 		},
 	})
+
+	activityInterface := graphql.NewInterface(graphql.InterfaceConfig{
+		Name:        "Activity",
+		Description: "Activity for LPDP Member",
+		Fields: graphql.Fields{
+			"ID": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.String),
+				Description: "activity id",
+			},
+			"CreatedAt": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.DateTime),
+				Description: "creation date of activity",
+			},
+			"UpdatedAt": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.DateTime),
+				Description: "update date of activity",
+			},
+			"DeletedAt": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.DateTime),
+				Description: "deletion date of activity",
+			},
+			"Message": &graphql.Field{
+				Type:        graphql.String,
+				Description: "message of activity",
+			},
+			"Type": &graphql.Field{
+				Type:        graphql.String,
+				Description: "type of activity",
+			},
+			"OwnerRef": &graphql.Field{
+				Type:        graphql.String,
+				Description: "owner of activity",
+			},
+		},
+	})
+
 	userInterface := graphql.NewInterface(graphql.InterfaceConfig{
 		Name:        "User",
 		Description: "Member of LPDP",
@@ -64,6 +109,9 @@ func GetSchema() (graphql.Schema, error) {
 		Fields: graphql.Fields{
 			"achievement": &graphql.Field{
 				Type: achievementInterface,
+			},
+			"activity": &graphql.Field{
+				Type: activityInterface,
 			},
 			"user": &graphql.Field{
 				Type: userInterface,
