46d566dfe537248f800a586d9c23bc7867ea91da
Eric Laugier <ela@egen.guru>
2018-10-28 09:49:21 +0100

    update graphql schema

--- pkg/graphqllpdp/graphqllpdp.go
+++ pkg/graphqllpdp/graphqllpdp.go
@@ -10,9 +10,31 @@ var (
 
 // GetSchema ...
 func GetSchema() (graphql.Schema, error) {
+	achievementInterface := graphql.NewInterface(graphql.InterfaceConfig{
+		Name:        "Achievement",
+		Description: "Achievement for LPDP Member",
+		Fields: graphql.Fields{
+			"ID": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.String),
+				Description: "achievement id",
+			},
+			"CreatedAt": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.DateTime),
+				Description: "creation date of achievement",
+			},
+			"UpdatedAt": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.DateTime),
+				Description: "update date of achievement",
+			},
+			"DeletedAt": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.DateTime),
+				Description: "deletion date of achievement",
+			},
+		},
+	})
 	userInterface := graphql.NewInterface(graphql.InterfaceConfig{
 		Name:        "User",
-		Description: "User of LPDP",
+		Description: "Member of LPDP",
 		Fields: graphql.Fields{
 			"ID": &graphql.Field{
 				Type:        graphql.NewNonNull(graphql.String),
@@ -26,12 +48,23 @@ func GetSchema() (graphql.Schema, error) {
 				Type:        graphql.NewNonNull(graphql.DateTime),
 				Description: "update date of user",
 			},
+			"DeletedAt": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.DateTime),
+				Description: "deletion date of user",
+			},
+			"Email": &graphql.Field{
+				Type:        graphql.String,
+				Description: "email address of user",
+			},
 		},
 	})
 
 	queryType := graphql.NewObject(graphql.ObjectConfig{
 		Name: "Query",
 		Fields: graphql.Fields{
+			"achievement": &graphql.Field{
+				Type: achievementInterface,
+			},
 			"user": &graphql.Field{
 				Type: userInterface,
 			},
