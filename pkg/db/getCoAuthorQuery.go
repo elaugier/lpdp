477cf202749e9a81e6c6036ecbfce7731f6341f1
Eric Laugier <ela@egen.guru>
2019-02-17 22:41:53 +0100

    update travis ci

---
+++ pkg/db/getCoAuthorQuery.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"math"
+
+	"github.com/elaugier/lpdp/pkg/models"
+	"github.com/graphql-go/graphql"
+)
+
+//GetCoAuthorQuery ...
+func GetCoAuthorQuery(params graphql.ResolveParams) (interface{}, error) {
+	var coauthors []models.CoAuthor
+	var count int
+	page := params.Args["page"].(int)
+	logger.Printf("page requested : %d", page)
+	itemsPerPage := params.Args["itemsPerPage"].(int)
+	logger.Printf("number of items per page requested : %d", itemsPerPage)
+	Inst.c.Model(&models.CoAuthor{}).Count(&count)
+	pages := math.RoundToEven(float64(count) / float64(itemsPerPage))
+	logger.Printf("computed total pages : %d", int(pages))
+	offset := (page - 1) * itemsPerPage
+	Inst.c.Offset(offset).Limit(itemsPerPage).Find(&coauthors)
+	return coauthors, nil
+}
